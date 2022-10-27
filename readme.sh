d057a690ee0b30f423a2b044cc81837afd15d64a
GTB3NW <GTB3NW@users.noreply.github.com>
2022-10-28 00:11:25 +0100

    Improve readme

--- readme.sh
+++ readme.sh
@@ -10,9 +10,9 @@ rm README.md
 echo "# Minecraft BDS versions" >> README.md
 echo "" >> README.md
 echo "<table align=\"right\">" >> README.md
-echo "  <tr><th colspan="2"><strong>Latest Version</strong></th></tr>" >> README.md
-echo "  <tr><th><strong>Linux</strong></th><th><strong>Windows</strong></th></tr>" >> README.md
-echo "  <tr><td>$LINUX_STABLE_VERSION</td><td>$WINDOWS_STABLE_VERSION</td></tr>" >> README.md
+echo "  <tr>Version<th></th><th><strong>Linux</strong></th><th><strong>Windows</strong></th></tr>" >> README.md
+echo "  <tr><strong>Stable</strong><td>$LINUX_STABLE_VERSION</td><td>$WINDOWS_STABLE_VERSION</td></tr>" >> README.md
+echo "  <tr><strong>Preview</strong><td>$LINUX_PREVIEW_VERSION</td><td>$WINDOWS_PREVIEW_VERSION</td></tr>" >> README.md
 echo "</table>" >> README.md
 echo "" >> README.md
 cat ./README/core.header >> README.md

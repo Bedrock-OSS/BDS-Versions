bd99fd3905598d6f8bc69304689f541a24f9a9d8
GTB3NW <GTB3NW@users.noreply.github.com>
2022-10-28 00:28:44 +0100

    Fix error

--- readme.sh
+++ readme.sh
@@ -12,7 +12,7 @@ echo "" >> README.md
 echo "<table align=\"right\">" >> README.md
 echo "  <tr>Version<th></th><th><strong>Linux</strong></th><th><strong>Windows</strong></th></tr>" >> README.md
 echo "  <tr><td><strong>Stable</strong></td><td>$LINUX_STABLE_VERSION</td><td>$WINDOWS_STABLE_VERSION</td></tr>" >> README.md
-echo "  <tr><td><<strong>Preview</strong></td><td>$LINUX_PREVIEW_VERSION</td><td>$WINDOWS_PREVIEW_VERSION</td></tr>" >> README.md
+echo "  <tr><td><strong>Preview</strong></td><td>$LINUX_PREVIEW_VERSION</td><td>$WINDOWS_PREVIEW_VERSION</td></tr>" >> README.md
 echo "</table>" >> README.md
 echo "" >> README.md
 cat ./README/core.header >> README.md

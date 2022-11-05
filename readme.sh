b5a1f201d10bfdce249c452bf5cf1308a89149d7
GTB3NW <GTB3NW@users.noreply.github.com>
2022-11-05 16:56:23 +0000

    Update readme.sh

--- readme.sh
+++ readme.sh
@@ -9,7 +9,7 @@ rm README.md
 
 echo "# Minecraft BDS versions" >> README.md
 echo "" >> README.md
-echo "<table class="margin-left: 1rem;" align=\"right\">" >> README.md
+echo "<table align=\"right\">" >> README.md
 echo "  <tr>Version<th></th><th><strong>Linux</strong></th><th><strong>Windows</strong></th></tr>" >> README.md
 echo -e  "<tr><td><strong>Stable</strong></td>\n<td>\n\n\`\`\`bash\n$LINUX_STABLE_VERSION\n\`\`\`\n\n</td>\n<td>\n\n\`\`\`bash\n$WINDOWS_STABLE_VERSION\n\`\`\`\n\n</td>\n</tr>" >> README.md
 echo -e  "<tr><td><strong>Preview</strong></td>\n<td>\n\n\`\`\`bash\n$LINUX_PREVIEW_VERSION\n\`\`\`\n\n</td>\n<td>\n\n\`\`\`bash\n$WINDOWS_PREVIEW_VERSION\n\`\`\`\n\n</td>\n</tr>" >> README.md

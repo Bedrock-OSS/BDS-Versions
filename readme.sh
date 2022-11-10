df0a904e4747c126e8a847bf200da9f59d662780
GTB3NW <GTB3NW@users.noreply.github.com>
2022-11-10 22:35:04 +0000

    Change highlighting

--- readme.sh
+++ readme.sh
@@ -11,8 +11,8 @@ echo "# Minecraft BDS versions" >> README.md
 echo "" >> README.md
 echo "<table align=\"right\">" >> README.md
 echo "  <tr>Version<th></th><th><strong>Linux</strong></th><th><strong>Windows</strong></th></tr>" >> README.md
-echo -e  "<tr><td><strong>Stable</strong></td>\n<td>\n\n\`\`\`bash\n$LINUX_STABLE_VERSION\n\`\`\`\n\n</td>\n<td>\n\n\`\`\`bash\n$WINDOWS_STABLE_VERSION\n\`\`\`\n\n</td>\n</tr>" >> README.md
-echo -e  "<tr><td><strong>Preview</strong></td>\n<td>\n\n\`\`\`bash\n$LINUX_PREVIEW_VERSION\n\`\`\`\n\n</td>\n<td>\n\n\`\`\`bash\n$WINDOWS_PREVIEW_VERSION\n\`\`\`\n\n</td>\n</tr>" >> README.md
+echo -e  "<tr><td><strong>Stable</strong></td>\n<td>\n<code>$LINUX_STABLE_VERSION</code>\n</td>\n<td>\n<code>$WINDOWS_STABLE_VERSION</code>\n</td>\n</tr>" >> README.md
+echo -e  "<tr><td><strong>Preview</strong></td>\n<td>\n<code>$LINUX_PREVIEW_VERSION</code>\n</td>\n<td>\n<code>$WINDOWS_PREVIEW_VERSION</code>\n</td>\n</tr>" >> README.md
 echo "</table>" >> README.md
 echo "" >> README.md
 cat ./README/core.header >> README.md
